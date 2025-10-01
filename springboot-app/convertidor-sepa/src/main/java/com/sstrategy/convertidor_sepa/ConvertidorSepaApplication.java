27338d626f224407076e59d31c405cdd62ca4ae4
Benjamin <brcj16@gmail.com>
2025-10-01 07:58:11 +0200

    directorios Spring Boot y Angular

---
+++ springboot-app/convertidor-sepa/src/main/java/com/sstrategy/convertidor_sepa/ConvertidorSepaApplication.java
@@ -0,0 +1,13 @@
+package com.sstrategy.convertidor_sepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertidorSepaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertidorSepaApplication.class, args);
+	}
+
+}
