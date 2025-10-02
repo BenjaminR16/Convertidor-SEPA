a0f4475bb01234af5a6163085fab9f4ae6db4e64
Benjamin <brcj16@gmail.com>
2025-10-02 09:51:34 +0200

    Add AppConfig.java

---
+++ springboot-app/convertidor-sepa/src/main/java/com/sstrategy/convertidor_sepa/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.sstrategy.convertidor_sepa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
