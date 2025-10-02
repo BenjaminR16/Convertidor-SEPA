ccc9107c37485b5950eb6b29921dda9f0141900d
rokas-es <rokas.vaivuckas@educa.madrid.org>
2025-10-02 12:35:11 +0200

    agregar header, footer y componente de carga de archivos (implementación parcial) en Angular

---
+++ angular-app/convertidor-sepa/src/app/components/layout/file-upload/file-upload.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload',
+  imports: [],
+  templateUrl: './file-upload.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FileUploadComponent { }
